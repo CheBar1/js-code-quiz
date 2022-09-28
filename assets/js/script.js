77dea9a280016614cb5daa1ed3ee64184e171484
CheBar1 <chebarclay@live.com.au>
2022-09-28 18:10:38 +0930

    added timer to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,25 +14,52 @@ HTML
 
 //When the game is over I can save my intials and my score
 
-var question = document.getElementById("question-title");
-var choices = Array.from(document.getElementById("choices"));
+// var question = document.getElementById("question-title");
+// var choices = Array.from(document.getElementById("choices"));
 
-var currentQuestion = {};
-var acceptingAnswers = true;
-var score = 0
-var questionCounter = 0;
-var availableQuestions = [];
+// var currentQuestion = {};
+// var acceptingAnswers = true;
+// var score = 0
+// var questionCounter = 0;
+// var availableQuestions = [];
 
-var questions = [
-    {
-        question: "Commonly used data types DO NOT include:",
-        choice1: "strings",
-        choice2: "booleans",
-        choice3: "alerts",
-        choice4: "numbers",
-        correntAnswer: 3
-    },
-    {
+// var questions = [
+//     {
+//         question: "Commonly used data types DO NOT include:",
+//         choice1: "strings",
+//         choice2: "booleans",
+//         choice3: "alerts",
+//         choice4: "numbers",
+//         correctAnswer: 3 // index - alerts
+//     },
+//     {
+//         question: "The condition in an if/else statement is enclosed within ____."
+//         choice1: "quotes" 
+//         choice2: "curly brackets"
+//         choice3: "parenthesis"
+//         choice4: "square brackets"
+//         correctAnswer: 3 // index - parenthesis
+//     }
+// ]
+
+var timerText = document.getElementById('timer-text');
+var startButton = document.getElementById('start-quiz');
+var secondsLeft = 75;
+
+// When start quiz button selected the timer starts to count down from 75 seconds
+function startTimer() {
+        var timeInterval = setInterval(function() {
+        secondsLeft--;
         
-    }
-]
+        if (secondsLeft > 1) {
+            timerText.textContent = secondsLeft;
+
+        } else if (timeLeft === 1) {
+            timerText.textContent = 'Times UP!';
+            clearInterval(timeInterval);
+        }
+  }, 1000);
+}
+
+startButton.addEventListener("click", startTimer) 
+ 
